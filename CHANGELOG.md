55c26cbd66098982d36b0d66912c8e4e4c375d15
Arman <armnet-10@mail.ru>
2022-12-07 23:39:48 +0400

    Add support for Inteliji 223.* versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,26 @@
-# Changelog
+<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+
+# Apricode Monokai Changelog
+
+## [1.1.0]
+
+### Added
+- support for IntelliJ Platform IDE 2022.3+
+
+## [1.0.3]
+
+### Fixed
+- color scheme version problem
+
+## [1.0.2]
+
+### Updated
+- Selected found text foreground color
 
 ## [1.0.1]
-- Updated injected language colors
+
+### Updated
+- Injected language colors
 
 ## [1.0.0]
 - First Release
-
--- src/main/kotlin/com/github/arm092/apricodemonokai/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/arm092/apricodemonokai/listeners/MyProjectManagerListener.kt
@@ -7,7 +7,10 @@ import com.github.arm092.apricodemonokai.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
-    override fun projectOpened(project: Project) {
-        project.service<MyProjectService>()
-    }
+//    override fun projectOpened(project: Project) {
+//        project.service<MyProjectService>()
+//
+//        System.getenv("CI")
+//            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+//    }
 }
